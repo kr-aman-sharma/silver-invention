a7f7a9c35d833026f7ede02a6d787609a9280ac2
Aman Kumar Sharma <iak.sharma0004@gmail.com>
2026-03-19 23:45:37 +0530

    fix navigation bar, margin for project

--- src/@types/types.d.ts
+++ src/@types/types.d.ts
@@ -2,7 +2,6 @@ type SocialIcon = {
   _id: string;
   iconName: string;
   target: string;
-  canContact?: boolean;
   iconType: 'material' | 'core'
 };
 
--- src/confs/social-conf.ts
+++ src/confs/social-conf.ts
@@ -4,7 +4,6 @@ const SocialConf: SocialIcon[] = [
     iconName: "mail",
     target:
       "mailto:iak.sharma0004@gmail.com?subject=I%20want%20to%20Connect&body=I%20want%20to%20Connect%20to%20you%20regarding%20..",
-    canContact: true,
     iconType: "material",
   },
   {
@@ -23,14 +22,12 @@ const SocialConf: SocialIcon[] = [
     _id: "hackerrank",
     iconName: "cib-hackerrank",
     target: "https://www.hackerrank.com/profile/iak_sharma0004",
-    canContact: false,
     iconType: "core",
   },
   {
     _id: "leetcode",
     iconName: "cib-leetcode",
     target: "https://leetcode.com/u/iak_sharma",
-    canContact: false,
     iconType: "core",
   },
 ];
